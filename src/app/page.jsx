b328342a171acde761df572ac30b30f91477e5ea
Prantor-Hawlader <prantor502@gmail.comm>
2023-06-27 15:59:17 +0600

    Reusable button added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button/Button"
 import styles from "./page.module.css"
 import Image from "next/image"
 import hero from "public/hero.gif"
@@ -8,12 +9,9 @@ export default function Home() {
 <div className={styles.item}>
   <h1 className={styles.title}>Better design for your digital products.</h1>
   <p className={styles.desc}>Turning your idea into reality. We bring together the team from the global tech industry.</p>
-  <button className={styles.button}>See our works</button>
+  <Button url="/portfolio" text="See our works" />
 
 </div> 
-
-
-
 
     <div className={styles.item}>
 <Image className={styles.img} src={hero} width={500} height={500} alt="hero"/>
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from "./page.module.css"
+const Button = ({text,url}) => {
+  return (
+    <Link href={url}><button className={styles.button}>{text} </button></Link>
+  )
+}
+
+export default Button
