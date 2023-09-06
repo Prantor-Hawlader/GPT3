6039922781cab7a8a94c43d9e935cca6acba2dfe
Prantor-Hawlader <prantor502@gmail.comm>
2023-09-06 21:56:53 +0600

    Initially completed

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,11 +3,23 @@ import connect from "@/utilities/db";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
-  //Fetch
+  const url = new URL(request.url);
+  const username = url.searchParams.get("username");
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database error", { status: 500 });
+  }
+};
+export const POST = async (request) => {
+  const body = await request.json();
+  const newPost = new Post(body);
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse("Post has been created", { status: 201 });
   } catch (err) {
     return new NextResponse("Database error", { status: 500 });
   }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,44 +3,98 @@ import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
-
+import useSWR from "swr"
+import Image from 'next/image'
 
 
 const Dashboard = () => {
-//   const [data,setData] = useState([])
-// const [err,setErr] = useState(false)
-// const [isLoading,setLoading] = useState(false)
-//   useEffect(() => {
-//     async function getData() {
-//       setLoading(true)
-//       const res = await fetch('https://jsonplaceholder.typicode.com/posts', { next: { revalidate: 10 } })
-    
-//       if (!res.ok) {
-//      setErr(true)
-//       }
-     
-//       const data = await res.json()
-//       setData(data)
-//       setLoading(false)
-//     }
-//   getData()
-//   },[])
-const session = useSession();
-const route = useRouter();
-  console.log(session)
+  
 
-  if(session.status === "loading"){return <p>...Loading</p>} 
+  
+  const route = useRouter();
+  const session = useSession();
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, mutate,error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+
+  console.log(data)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try{
+      await fetch("/api/posts", {
+        method: "POST",
+        body : JSON.stringify({
+          title,desc,img,content,username: session.data.user.name
+        })
+      })
+      mutate();
+      e.target.reset();
+    
+
+    }
+    catch(err){
+      console.log(err)
+    }};
+
+    const handleDelete = async (id) => {
+      try{
+       await fetch(`/api/posts/${id}`,{
+        method : "DELETE"
+       });
+       mutate();
+      }
+      catch(err){
+        console.log(err)
+      }
+
+    }
+
+
+  if(session.status === "loading")
+     {return <p>...Loading</p>} 
 
 if(session.status === "unauthenticated"){
  route?.push("/dashboard/login")
 }
+
+
 if(session.status === "authenticated"){
   return (
-    <div className={styles.container}>Dashboard</div>
+    <div className={styles.container}>
+
+<div className={styles.posts}>
+  {isLoading ?  "loading" : data.map((post)=>(<div className={styles.post} key={post._id}>
+  <div className={styles.imgContainer}>
+    <Image src={post.img} alt='' width={200} height={100} className={styles.img} />
+  </div>
+  <h2 className={styles.postTitle}>{post.title}</h2>
+  <span className={styles.delete} onClick={()=> handleDelete(post._id)}>X</span>
+  </div>) )}
+  
+</div>
+
+<form className={styles.new} onSubmit={handleSubmit}>
+  <h1>Add new post</h1>
+  <input type="text" placeholder='Title' className={styles.input} />
+  <input type="text" placeholder='Description' className={styles.input} />
+  <input type="text" placeholder='Image' className={styles.input} />
+  <textarea placeholder='Content' cols="30" rows="10" className={styles.textArea}></textarea>
+  <button className={styles.button}>Send</button>
+</form>
+
+    </div>
   )
 }
 
-
 }
+
 
 export default Dashboard
